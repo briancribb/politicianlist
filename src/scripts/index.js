a3515c320dd625cf61be68c0acb41e24375277c2
Brian Cribb <brian.cribb@gmail.com>
2021-09-06 07:19:21 -0400

    Reorganizing init

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -40,95 +40,14 @@ let POL = {
 
 		let housePromise = new Promise((resolve, reject) => {
 			this.getData(this.congress,'house').then((data)=>{
-				resolve(updateMembers(data.results[0]));
+				resolve(this.updateMembers(data.results[0]));
 			});
 		});
 		let senatePromise = new Promise((resolve, reject) => {
 			this.getData(this.congress,'senate').then((data)=>{
-				resolve(updateMembers(data.results[0]));
+				resolve(this.updateMembers(data.results[0]));
 			});
 		});
-
-		/*
-		ProPublica recently updated their data and wound up putting representatives into the 
-		array twice. The person's id then shows up twice in the array and makes React angry.
-		The two entries have different "last updated" dates, so when this happens we're going 
-		to just grab the last one.
-		*/
-		let reducer = (accumulator, currentValue) => {
-			if (currentValue > accumulator) accumulator = currentValue;
-			let accDate = accumulator ? new Date(accumulator.last_updated) : null,
-				currDate = new Date(currentValue.last_updated);
-
-			if (currDate.getTime() > accDate.getTime()) accumulator = currentValue;
-			return accumulator; 
-		}
-
-		/*
-		The object returned has some top-level stuff and an array of members. Moving these 
-		properties into each member. Less efficient, maybe, but we're never going to have 
-		thousands of members of Congress at once and this will make the array easier to mess
-		around with in React.
-		*/
-		function updateMembers (obj) {
-			let objParties = {
-				"R":"Republican",
-				"D":"Democrat",
-				"I":"Independent"
-			}
-
-			// Prep for duplicates and filter down to members who are currently in office.
-			let updatedMembers = [],
-				objDoubles = {},
-				everyone = obj.members.filter((item)=>{return item.in_office === true});
-
-			// Separate duplicate entries into objects, each contains an array of dupes.
-			everyone.forEach((member)=>{
-
-				// Party info from the object above, other info from the imported "states" object.
-				member.party_name = objParties[member.party] || "Independent";
-				member.state_name = states[member.state];
-				member.chamber = obj.chamber;
-
-				let birthDate = new Date(member.date_of_birth);
-				let today = new Date();
-				let age = today.getFullYear() - birthDate.getFullYear();
-				let m = today.getMonth() - birthDate.getMonth();
-				if (m < 0 || m === 0 && today.getDate() < birthDate.getDate() ) {
-					age--; // Hasn't had a birthday this year.
-				}
-				member.age = age;
-
-				// If it's a known missing image, use a local photo.
-				member.missing_photo = (!missing[member.id]) ? false : true;
-				member.member_photo_link = (!missing[member.id]) 
-				? 'https://theunitedstates.io/images/congress/225x275/'+ member.id +'.jpg'
-				: 'src/images/missing/225x275/'+ member.id +'.jpg'
-				;
-
-				if (everyone.filter((m)=>{return m.id===member.id}).length > 1) {
-					// Make an array if there isn't one yet, then add member to it.
-					objDoubles[member.id] = objDoubles[member.id] || [];
-					objDoubles[member.id].push(member);
-				} else {
-					// No dupes, just add to the final members array.
-					updatedMembers.push(member);
-				}
-			});
-
-			/*
-			Each object in this array contains duplicates. The dupes have different last_updated
-			values, so we use the reducer function above to grab the most recent one and add it 
-			to the updatedMembers array.
-
-			And voila! We now have the data that ProPublica should have given us in the first place!
-			*/
-			Object.keys(objDoubles).forEach((id)=>{
-				updatedMembers.push( objDoubles[id].reduce(reducer) );
-			});
-
-			return updatedMembers;
-		}
 
 		function setupUtils() {
 			tmc_RAF();// Polyfills the window.requestAnimationFrame object.
@@ -144,7 +63,7 @@ let POL = {
 		TODO: Get/set filters from local storage to reduce calls to the server. Use a timestamp 
 		to make sure we still call once a day, but we're probably going to have to refresh the 
 		page to get new query string parameters into the address bar. Then the user can just 
-		copy/paste the url to share a specific search.
+		copy/paste the url to share
 		*/
 		//console.log('this.utils.getParams()', this.utils.getParams());
 
@@ -155,11 +74,87 @@ let POL = {
 		});
 		//console.log('POL, RC', POL, RC);
 	},
+	updateMembers : function(obj) {
+		/*
+		The object returned has some top-level stuff and an array of members. Moving these 
+		properties into each member. Less efficient, maybe, but we're never going to have 
+		thousands of members of Congress at once and this will make the array easier to mess
+		around with in React.
+		*/
+		let objParties = {
+			"R":"Republican",
+			"D":"Democrat",
+			"I":"Independent"
+		}
+
+		// Prep for duplicates and filter down to members who are currently in office.
+		let updatedMembers = [],
+			objDoubles = {},
+			everyone = obj.members.filter((item)=>{return item.in_office === true});
+
+		// Separate duplicate entries into objects, each contains an array of dupes.
+		everyone.forEach((member)=>{
+
+			// Party info from the object above, other info from the imported "states" object.
+			member.party_name = objParties[member.party] || "Independent";
+			member.state_name = states[member.state];
+			member.chamber = obj.chamber;
+
+			let birthDate = new Date(member.date_of_birth);
+			let today = new Date();
+			let age = today.getFullYear() - birthDate.getFullYear();
+			let m = today.getMonth() - birthDate.getMonth();
+			if (m < 0 || m === 0 && today.getDate() < birthDate.getDate() ) {
+				age--; // Hasn't had a birthday this year.
+			}
+			member.age = age;
+
+			// If it's a known missing image, use a local photo.
+			member.missing_photo = (!missing[member.id]) ? false : true;
+			member.member_photo_link = (!missing[member.id]) 
+			? 'https://theunitedstates.io/images/congress/225x275/'+ member.id +'.jpg'
+			: 'src/images/missing/225x275/'+ member.id +'.jpg'
+			;
+
+			if (everyone.filter((m)=>{return m.id===member.id}).length > 1) {
+				// Make an array if there isn't one yet, then add member to it.
+				objDoubles[member.id] = objDoubles[member.id] || [];
+				objDoubles[member.id].push(member);
+			} else {
+				// No dupes, just add to the final members array.
+				updatedMembers.push(member);
+			}
+		});
+
+		/*
+		These arrays returned may contain duplicates, so run the reducer function from below.
+		*/
+		Object.keys(objDoubles).forEach((id)=>{
+			updatedMembers.push( objDoubles[id].reduce(reducer) );
+		});
+
+
+		/*
+		ProPublica recently updated their data and wound up putting representatives into the 
+		array twice. The person's id then shows up twice in the array and makes React angry.
+		The two entries have different "last updated" dates, so when this happens we're going 
+		to just grab the most recent one.
+
+		And voila! We now have the data that ProPublica should have given us in the first place!		
+		*/
+		var reducer = (accumulator, currentValue) => {
+			if (currentValue > accumulator) accumulator = currentValue;
+			let accDate = accumulator ? new Date(accumulator.last_updated) : null,
+				currDate = new Date(currentValue.last_updated);
+
+			if (currDate.getTime() > accDate.getTime()) accumulator = currentValue;
+			return accumulator; 
+		}
+
+		return updatedMembers;
+	},
 	getAllMembers : function() {
 		return this.senate.members.concat(this.house.members);
-	},
-	getQueryParams : function() {
-
 	},
 	getData : function(congress, chamber) {
 		return $.ajax({
