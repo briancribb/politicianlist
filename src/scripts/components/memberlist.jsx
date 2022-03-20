b0843a03ae7b4ac2701086b99ca943871e37db28
Brian Cribb <brian.cribb@gmail.com>
2022-03-20 06:41:01 -0400

    Better function names and arrangement.

--- src/scripts/components/memberlist.jsx
+++ src/scripts/components/memberlist.jsx
@@ -12,31 +12,26 @@ let memberList = class extends React.Component {
 			modalView: 	'filters'	
 		};
 
-		this._assembleFilters 		= this._assembleFilters.bind(this);
-		this._updateSort	 		= this._updateSort.bind(this);
-		this._reverseSort 			= this._reverseSort.bind(this);
-		this._updateFilter 			= this._updateFilter.bind(this);
-		this._resetFilters 			= this._resetFilters.bind(this);
-		this._passedFilters 		= this._passedFilters.bind(this);
-		this._getIconClass 			= this._getIconClass.bind(this);
-		this._getFilteredSorted 	= this._getFilteredSorted.bind(this);
-		this._getMemberItems 		= this._getMemberItems.bind(this);
-		this._getMemberName 		= this._getMemberName.bind(this);
-		this._handleLaunchModal 	= this._handleLaunchModal.bind(this);
-		this._handleImageError 		= this._handleImageError.bind(this);
-		this._handleInputChange 	= this._handleInputChange.bind(this);
+		let arrFunctions = [
+			"assembleFilters","updateSort","reverseSort","updateFilter","resetFilters",
+			"passedFilters","getIconClass","getFilteredSorted","getMemberItems",
+			"getMemberName","handleLaunchModal","handleImageError","handleInputChange"
+		];
+		arrFunctions.forEach((name, i) => {
+			this[name] = this[name].bind(this);
+		});
 	}
 
 	componentDidMount() {
 		this.setState({
-			filters:this._assembleFilters()
+			filters:this.assembleFilters()
 		});
 		window.getState = ()=>{
 			return this.state;
 		};
 	}
 
-	_assembleFilters() {
+	assembleFilters() {
 		let objTags = {
 			next_election:{},
 			party_name:{},
@@ -51,17 +46,17 @@ let memberList = class extends React.Component {
 		return objTags;
 	}
 
-	_updateSort(sortBy = null) {
+	updateSort(sortBy = null) {
 		if (!sortBy || sortBy === this.state.sortBy) return;
 		this.setState({sortBy});// ES6 shorthand for {sortBy:sortBy}
 	}
 
-	_reverseSort() {
+	reverseSort() {
 		let reverse = this.state.reverse ? false : true; // Opposite of the current state.
 		this.setState({reverse});// ES6 shorthand for {reverse:reverse}
 	}
 
-	_updateFilter(category = null, filter = null, value = null) {
+	updateFilter(category = null, filter = null, value = null) {
 		if (!category || !filter) return;
 		this.setState((prevState)=>{
 			let filters = { ...prevState.filters }; // Copy the filters object from the state. ES6 shorthand for Object.assign()
@@ -78,7 +73,7 @@ let memberList = class extends React.Component {
 		});
 	}
 
-	_resetFilters() {
+	resetFilters() {
 		// Sets all filters to false.
 		this.setState((prevState)=>{
 			let filters = { ...prevState.filters };
@@ -91,7 +86,7 @@ let memberList = class extends React.Component {
 		});
 	}
 
-	_getIconClass(party) {
+	getIconClass(party) {
 		let objClasses = {
 			"R":"fas fa-republican",
 			"D":"fas fa-democrat",
@@ -100,7 +95,7 @@ let memberList = class extends React.Component {
 		return objClasses[party] || objClasses['I'];
 	}
 
-	_passedFilters(member) {
+	passedFilters(member) {
 		/*
 		Run logic to see if a member should be shown.
 		Calling these properties categories and filters just to organize them in my brain.
@@ -177,9 +172,9 @@ let memberList = class extends React.Component {
 	/*
 	Returns a new array that passes filter tests and is then sorted.
 	*/
-	_getFilteredSorted() {
+	getFilteredSorted() {
 		let members = this.state.members.filter((member)=>{
-			return this._passedFilters(member);
+			return this.passedFilters(member);
 		});
 
 		members.sort((a,b)=>{
@@ -199,13 +194,13 @@ let memberList = class extends React.Component {
 		return members;
 	}
 
-	_getMemberName(member) {
+	getMemberName(member) {
 		return member.short_title + ' ' + member.first_name + ' ' + member.last_name;
 	}
 
-	_getMemberItems(chamber = 'both') {
+	getMemberItems(chamber = 'both') {
 
-		let members = this._getFilteredSorted(); // This will return a new filtered and sorted array of members.
+		let members = this.getFilteredSorted(); // This will return a new filtered and sorted array of members.
 
 		return members.map((member) =>{
 			let partyColor = 'success';
@@ -221,21 +216,21 @@ let memberList = class extends React.Component {
 			return(
 				<div data-member-id={member.id} className={'member card mb-3 border border-'+partyColor} key={member.id}>
 					<div className={'card-header bg-'+partyColor+' text-white d-sm-flex justify-content-between'}>
-						<div className="party"><i className={this._getIconClass(member.party) + ' mr-2'}></i>
+						<div className="party"><i className={this.getIconClass(member.party) + ' mr-2'}></i>
 						&nbsp;{member.party_name}</div>
 						<div className="next">Next: <strong>{member.next_election}</strong></div>
 					</div>
 					<div className="card-body">
 						<div className="d-sm-flex">
 							<div className="member-photo-wrapper mb-2 mb-sm-0">
-								<img className="member-photo img-fluid mx-auto ml-sm-0" onError={this._handleImageError} src={member.member_photo_link} loading="lazy" alt={'Photo of ' + this._getMemberName(member)} />
+								<img className="member-photo img-fluid mx-auto ml-sm-0" onError={this.handleImageError} src={member.member_photo_link} loading="lazy" alt={'Photo of ' + this.getMemberName(member)} />
 								<div className="missing-photo text-center">
 									<i className="fas fa-user mb-3 text-secondary"></i>
 									<small className="d-block">Photo not available</small>
 								</div>
 							</div>
 							<div className="member-info-wrapper">
-								<h5 className="card-title mb-1">{this._getMemberName(member)}</h5>
+								<h5 className="card-title mb-1">{this.getMemberName(member)}</h5>
 								<div className="mb-0">{member.state_name}</div>
 								<div className="mb-0">{'Age: '+member.age}</div>
  							</div>
@@ -246,19 +241,19 @@ let memberList = class extends React.Component {
 		});
 	}
 
-	_handleImageError(evt) {
+	handleImageError(evt) {
 		evt.target.parentElement.classList.add('image-error');
 	}
 
-	_handleLaunchModal(evt) {
+	handleLaunchModal(evt) {
 		this.setState({modalView:evt.target.dataset.view});
 		$('#myModal').modal('show');
 	}
 
-	_handleInputChange(evt) {
+	handleInputChange(evt) {
 		let value = evt.target.value;
 		if(value.length < 3) value = null;
-		this._updateFilter('text', 'name', value);
+		this.updateFilter('text', 'name', value);
 	}
 
 	render() {
@@ -270,23 +265,23 @@ let memberList = class extends React.Component {
 			<div className="row">
 				<div className="col">
 					<div className="btn-group btn-group-lg w-100 mb-3" role="group" aria-label="Modal launch buttons">
-						<button type="button" className="d-inline-block btn btn-light flex-fill border border-dark w-50" data-view="filters" onClick={that._handleLaunchModal}>Filters</button>
-						<button type="button" className="d-inline-block btn btn-light flex-fill border border-dark w-50" data-view="sorting" onClick={that._handleLaunchModal}>Sorting</button>
+						<button type="button" className="d-inline-block btn btn-light flex-fill border border-dark w-50" data-view="filters" onClick={that.handleLaunchModal}>Filters</button>
+						<button type="button" className="d-inline-block btn btn-light flex-fill border border-dark w-50" data-view="sorting" onClick={that.handleLaunchModal}>Sorting</button>
 					</div>
 				</div>
 			</div>
 			<div className="row">
 				<div className="col">
-					<input className="form-control form-control-lg" type="text" placeholder="Filter by name" onChange={this._handleInputChange}/>
+					<input className="form-control form-control-lg" type="text" placeholder="Filter by name" onChange={this.handleInputChange}/>
 					<small className="d-block mt-1 mb-3 px-3"><em>Filtering by name requires at least three characters.</em></small>
 				</div>
 			</div>
 			<div className="row">
 				<div className="col">
-					{this._getMemberItems('both')}
+					{this.getMemberItems('both')}
 				</div>
 			</div>
-			<RC.modal updateSort={this._updateSort} reverseSort={this._reverseSort} updateFilter={this._updateFilter} resetFilters={this._resetFilters} modalView={that.state.modalView} reverse={that.state.reverse} sortBy={that.state.sortBy} filters={that.state.filters} />
+			<RC.modal updateSort={this.updateSort} reverseSort={this.reverseSort} updateFilter={this.updateFilter} resetFilters={this.resetFilters} modalView={that.state.modalView} reverse={that.state.reverse} sortBy={that.state.sortBy} filters={that.state.filters} />
 		</div>
 		);
 	}
