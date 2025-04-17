25d100d4db12bd68e9b4b4af927a1838808dfeb0
Brian Cribb <brian.cribb@gmail.com>
2025-04-17 13:05:27 -0400

    Starting to work with the data.

--- src/MemberList.jsx
+++ src/MemberList.jsx
@@ -1,12 +1,27 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './MemberList.scss';
 import utils from './assets/utils';
 import memberListData from './assets/memberListData';
 
 function MemberList() {
   console.log({utils,memberListData});
+  const [memberData] = useState(memberListData.members);
+
+
+  const getMembers = ()=>{
+    let listItems = memberData.map((member)=>{
+      return (<li>{member.directOrderName}</li>);
+    });
+    return (<ul>{listItems}</ul>)
+  }
+
+
+
+
+
+
+
+
   return (
     <>
       <div className="container pt-5">
@@ -14,6 +29,7 @@ function MemberList() {
         <p className="lead">A list of current members of Congress</p>
         <p>I need to build something to show off some modern code, so I just grabbed the array of members from three direct calls in the browser and imported it. This is enough to get started.</p>
         <p>Here's the <a target="_blank" href="https://github.com/briancribb/gather-congress/">GitHub repo</a> for this utility.</p>
+        {getMembers()}
       </div>
     </>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import MemberList from './MemberList.jsx'
 
+/*
+Not sure if I really need a context with this thing, but I might 
+add one just to show that I know how.
+*/
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <MemberList />
