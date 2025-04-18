3c8129dff699f82509589f6811c68134170f2d9d
Brian Cribb <brian.cribb@gmail.com>
2025-04-18 10:41:47 -0400

    Mining old code and matching to new data.

--- src/MemberList.jsx
+++ src/MemberList.jsx
@@ -2,18 +2,68 @@ import { useState } from 'react';
 import './MemberList.scss';
 import utils from './assets/utils';
 import memberListData from './assets/memberListData';
-import testData from './assets/transform';
 
 function MemberList() {
-  console.log({utils,memberListData,testData});
+  console.log({utils,memberListData});
   const [memberData] = useState(memberListData.members);
+
+  const handleImageError = (evt)=>{
+    console.log("Image error", {evt});
+  }
+
+  const getIconClass = (party)=>{
+    let objClasses = {
+      "R":"fas fa-republican",
+      "D":"fas fa-democrat",
+      "I":"fas fa-flag-usa"
+    }
+    return objClasses[party] || objClasses['I'];
+  }
+
 
 
   const getMembers = ()=>{
-    let listItems = memberData.map((member)=>{
-      return (<li>{member.directOrderName}</li>);
+    let listItems = memberData.map((member, index)=>{
+
+
+      let partyColor = 'success';
+      switch(member.partyAbbr) {
+        case "R":
+          partyColor = 'danger'
+          break;
+        case "D":
+          partyColor = 'primary'
+          break;
+      }
+
+
+      return (
+        <div key={`key_${member.id}`} className={'member card mb-3 border border-'+partyColor}>
+          <div className={'card-header bg-'+partyColor+' text-white d-sm-flex justify-content-between'}>
+            <div className="party"><i className={getIconClass(member.partyAbbr) + ' mr-2'}></i>
+            &nbsp;{member.partyName}</div>
+            <div className="next">Start: <strong>{member.startYear}</strong></div>
+          </div>
+          <div className="card-body">
+            <div className="d-sm-flex">
+              <div className="member-photo-wrapper mb-2 mb-sm-0">
+                <img className="member-photo img-fluid mx-auto ml-sm-0" onError={handleImageError} src={member.imageUrl || ""} loading="lazy" alt={`Photo of ${member.fullName}`} />
+                <div className="missing-photo text-center">
+                  <i className="fas fa-user mb-3 text-secondary"></i>
+                  <small className="d-block">Photo not available</small>
+                </div>
+              </div>
+              <div className="member-info-wrapper">
+                <h5 className="card-title mb-1">{member.fullName}</h5>
+                <div className="mb-0">{member.state_name}</div>
+                <div className="mb-0">{'Birth Year: '+member.birthYear}</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
     });
-    return (<ul>{listItems}</ul>)
+    return (listItems)
   }
 
 
@@ -30,8 +80,37 @@ function MemberList() {
         <p className="lead">A list of current members of Congress</p>
         <p>I need to build something to show off some modern code, so I just grabbed the array of members from three direct calls in the browser and imported it. This is enough to get started.</p>
         <p>Here's the <a target="_blank" href="https://github.com/briancribb/gather-congress/">GitHub repo</a> for this utility.</p>
-        {getMembers()}
       </div>
+
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <div className="btn-group btn-group-lg w-100 mb-3" role="group" aria-label="Modal launch buttons">
+              <button type="button" className="d-inline-block btn btn-light flex-fill border border-dark w-50" data-view="filters" onClick={()=>{console.log("Clicked",this)}}>Filters</button>
+              <button type="button" className="d-inline-block btn btn-light flex-fill border border-dark w-50" data-view="sorting" onClick={()=>{console.log("Clicked",this)}}>Sorting</button>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <input className="form-control form-control-lg" type="text" placeholder="Filter by name" onChange={()=>{console.log("Changed",this)}}/>
+            <small className="d-block mt-1 mb-3 px-3"><em>Filtering by name requires at least three characters.</em></small>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            {getMembers()}
+          </div>
+        </div>
+      </div>
+
+
+
+
+
+
+
+
     </>
   )
 }
