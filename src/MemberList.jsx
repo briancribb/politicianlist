33bd0556b4990d7c99a76eaf6e6a16a920085166
Brian Cribb <brian.cribb@gmail.com>
2025-04-22 15:42:46 -0400

    Working on filter and sort

--- src/MemberList.jsx
+++ src/MemberList.jsx
@@ -5,7 +5,7 @@ import Modal from './Modal';
 
 export default function MemberList() {
   console.log({memberListData});
-  const [arrMembers] = useState(memberListData.members);
+  const [allMembers] = useState(memberListData.members);
   const [modalOpen, setModalOpen] = useState(false);
   const [textMatch, setTextMatch] = useState("");
   const [filters, setFilters] = useState([]);
@@ -13,6 +13,11 @@ export default function MemberList() {
 
   const handleTextSearch = (evt)=>{
     console.log("handleTextSearch", evt.target.value);
+    setTextMatch(evt.target.value);
+  }
+  const handleModalClose = (evt)=>{
+    setTextMatch("");
+    setModalOpen(false);
   }
 
   useEffect(()=>{
@@ -38,13 +43,28 @@ export default function MemberList() {
   }
 
 
-  const getFilteredMembers = (filters = {})=>{
-    return arrMembers;
+  const getFilteredAndSorted = ()=>{
+    let filteredMembers = allMembers.filter((member)=> {
+      let pass = true;
+      if (textMatch.length >= 3 && !member.lastNameFirst.toLowerCase().includes( textMatch.toLowerCase() )) {
+        pass = false;
+      }
+
+
+
+
+
+      return pass;
+    });
+
+    let sortedMembers = filteredMembers;
+
+    return sortedMembers;
   }
 
   const getMembers = ()=>{
 
-    return getFilteredMembers().map((member, index)=>{
+    return getFilteredAndSorted().map((member, index)=>{
       let typeAbbr = member.type === "Senator" ? "Senator" : "Rep";
       let partyColor = 'success';
       switch(member.partyAbbr) {
@@ -90,7 +110,7 @@ export default function MemberList() {
 
   const getModal = ()=>{
     if (!modalOpen) return null;
-    return <Modal setModalOpen={setModalOpen} setSortBy={setSortBy} />
+    return <Modal handleModalClose={handleModalClose} setSortBy={setSortBy} />
   }
 
 
@@ -107,13 +127,13 @@ export default function MemberList() {
         <div className="row">
           <div className="col">
             <div className="btn-group btn-group-lg w-100 mb-3" role="group" aria-label="Modal launch buttons">
-              <button type="button" className="d-inline-block btn btn-light flex-fill border border-dark w-50" data-view="filters" onClick={()=>{setModalOpen(true)}}>Filter and Sort</button>
+              <button type="button" className="d-inline-block btn btn-light flex-fill border border-dark w-50" onClick={()=>{setModalOpen(true)}}>Filter and Sort</button>
             </div>
           </div>
         </div>
         <div className="row">
           <div className="col">
-            <input className="form-control form-control-lg" type="text" placeholder="Filter by name" onChange={handleTextSearch}/>
+            <input value={textMatch} className="form-control form-control-lg" type="text" placeholder="Filter by name" onChange={handleTextSearch}/>
             <small className="d-block mt-1 mb-3 px-3"><em>Filtering by name requires at least three characters.</em></small>
           </div>
         </div>
@@ -124,14 +144,6 @@ export default function MemberList() {
         </div>
       </div>
       {getModal()}
-
-
-
-
-
-
-
-
     </>
   )
 }
--- src/Modal.jsx
+++ src/Modal.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 
 function Modal(props) {
   console.log("Modal()");
-  const { setModalOpen,setSortBy } = props;
+  const { handleModalClose, setSortBy } = props;
 
   useEffect(()=>{
     console.log("-- Modal() - useEffect()");
@@ -13,22 +13,51 @@ function Modal(props) {
   const handleCloseClick = (evt)=>{
     if (["modal","btn-close", "apply"].includes(evt.target.id)) {
       console.log("handleCloseClick()", {props, id:evt.target.id});
-      setModalOpen(false);
+      handleModalClose();
     }
   }
 
 
   return (
     <>
-      <div className="modal fade show" id="modal" onClick={handleCloseClick} tabindex="-1" aria-labelledby="exampleModalLiveLabel" aria-modal="true" role="dialog">
-        <div className="modal-dialog">
+      <div className="modal fade show" id="modal" onClick={handleCloseClick} tabIndex="-1" aria-labelledby="exampleModalLiveLabel" aria-modal="true" role="dialog">
+        <div className="modal-dialog modal-dialog-scrollable">
           <div className="modal-content">
             <div className="modal-header">
-              <h1 className="modal-title fs-5" id="exampleModalLiveLabel">Filter and Sort</h1>
+              <h1 className="modal-title fs-5" id="exampleModalLiveLabel">Sort and Filter</h1>
               <button type="button" id="btn-close" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div className="modal-body">
-              <p>Woo-hoo, you're reading this text in a modal!</p>
+              <div className="mb-4">
+                <label htmlFor="sort-by" class="form-label">Sort By</label>
+                <select id="sort-by" placeholder="Sort by" className="form-select mb-2" aria-label="Default select example">
+                  <option value="0" defaultValue>Last Name</option>
+                  <option value="1">Age</option>
+                  <option value="2">Next Election</option>
+                </select>
+                <div className="form-check form-switch">
+                  <input className="form-check-input" type="checkbox" role="switch" id="reverse" />
+                  <label className="form-check-label" htmlFor="reverse">Reverse order</label>
+                </div>
+              </div>
+
+
+
+
+              <h2 className="modal-title fs-3">Filter By</h2>
+              <div className="btn-group w-100 mb-1" role="group" aria-label="Modal launch buttons">
+                <button type="button" className="btn btn-outline-primary w-50">Senate</button>
+                <button type="button" className="btn btn-outline-primary w-50">House</button>
+              </div>
+              <div className="btn-group w-100 mb-1" role="group" aria-label="Modal launch buttons">
+                <button type="button" className="btn btn-outline-primary w-50">Republican</button>
+                <button type="button" className="btn btn-outline-primary w-50">Democrat</button>
+              </div>
+              <div className="btn-group w-100 mb-3" role="group" aria-label="Modal launch buttons">
+                <button type="button" className="btn btn-outline-primary">2026</button>
+                <button type="button" className="btn btn-outline-primary">2028</button>
+                <button type="button" className="btn btn-outline-primary">2030</button>
+              </div>
             </div>
             <div className="modal-footer">
               <button id="apply" type="button" className="btn btn-primary">Apply</button>
